64f0fcdf23cfbe76bd25b4de6551430a55b0365b
sharif24 <azizur15-2124@diu.edu.bd>
2022-08-30 03:39:30 -0700

    UI Complete

---
+++ app/src/main/java/com/jovanovic/stefan/sqlitetutorial/SpActivity.java
@@ -0,0 +1,23 @@
+package com.jovanovic.stefan.sqlitetutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SpActivity.this,MainActivity.class));
+            }
+        },3000);
+    }
+}
